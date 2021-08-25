73754fb207fc955a024a8757184c723a5daa7565
simess101 <89533702+simess101@users.noreply.github.com>
2021-08-25 11:13:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************
+* Java Program Hello
+****************************************
+* Shane Simes
+* 8/25/2021
+* CMSC-255-004
+****************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Shane");
+		System.out.println("Hello");
+		System.out.println("This program prints a students name as well as hello; this program is meant to introduce the student to coding by showing the layout of code.");
+		System.out.println("8/25/2021");
+		System.out.println("CMSC-255-004");
+		System.out.println();
+	}
+}
